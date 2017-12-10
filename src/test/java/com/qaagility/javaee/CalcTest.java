ccd695decc805cddcae45e6c39767d871092d581
sunketIngale <sunket.ingale@qaagility.com>
2017-12-10 16:53:32 +0530

    Test cases commit after correction

--- src/test/java/com/qaagility/javaee/CalcTest.java
+++ src/test/java/com/qaagility/javaee/CalcTest.java
@@ -23,7 +23,7 @@ public class CalcTest{
   @Test
   public void testCalc(){
     //assertEquals("Result",9,9);
-    assertEquals("Result",18,new calc().mul());
+    assertEquals("Result",18,new Calc().mul());
     //System.out.println("TestCalc =" +9*9);
   }
   
