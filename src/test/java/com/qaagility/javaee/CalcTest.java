fe24518db70d87346e8a7c99c6e146deb9adad32
sunketIngale <sunket.ingale@qaagility.com>
2017-12-10 16:46:34 +0530

    Test cases commit

---
+++ src/test/java/com/qaagility/javaee/CalcTest.java
@@ -0,0 +1,36 @@
+package com.qaagility.javaee;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+
+public class CalcTest
+  public void testSlow(){
+    System.out.println("Slow");
+  }
+  
+  @Before
+  public void testSlower(){
+    System.out.println("Before");
+  }
+  
+  @After
+  public void testFast(){
+    System.out.println("After");
+  }
+  
+  @Test
+  public void testCalc(){
+    //assertEquals("Result",9,9);
+    assertEquals("Result",18,new calc().mul());
+    //System.out.println("TestCalc =" +9*9);
+  }
+  
+  @Test
+  public void testCalcFail(){
+    //assertEquals("Result",9,9);
+    assertEquals("Result",18,new calc().mul());
+    //System.out.println("TestCalc =" +9*9);
+}
+}
